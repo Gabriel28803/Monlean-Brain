74bd9f16732d90adeeda40b2c510d4151c78aa1a
Monascar <gabrielmonascars@gmail.com>
2021-06-26 13:08:21 -0430

    mini menu terminado, su movimiento es horizontal, faltan url a varios a del HTML

--- cuadros1.js
+++ cuadros1.js
@@ -1,3 +1,12 @@
+/*verificacion de tamaño de pantalla, si es inferior a cierta cantidad de px, quito los cuadros*/
+/*Aqui el contenedor de los cuadros de informacion*/
+let pagina1 = document.querySelector(".pagina1")
+let contenedorCuadros = document.querySelector(".pagina1__container--cuadros");
+if (window.screen.height <= 730){
+	contenedorCuadros.style.display= "none";
+	pagina1.style.flexDirection = "column";
+}
+
 /*Aqui selecciono las flechas*/
 
 /*Flechas de la pagina 1*/
@@ -22,41 +31,39 @@ let cuadro3 = document.querySelector(".pagina1__cuadro3");
 let cuadros = [cuadro1, cuadro2, cuadro3];
 let desplazamiento = 0;
 
-/*Aqui el contenedor de los cuadros de informacion*/
-let contenedorCuadros = document.querySelector(".pagina1__container--cuadros");
 
 /*Funciones para hacer que los cuadros se muevan*/
-const moverArriba = ()=>{
+const moverIzquierda = ()=>{
 	if (abierto){
 		cerrarTodo();
 	}
-	desplazamiento += 240;
+	desplazamiento += 350;
 	for (cuadro in cuadros){
-		cuadros[cuadro].style.transform = `translateY(${desplazamiento}px)`;
+		cuadros[cuadro].style.transform = `translateX(${desplazamiento}px)`;
 	}
 	
 }
-const moverAbajo = ()=>{
+const moverDerecha = ()=>{
 	if (abierto){
 		cerrarTodo();
 	}
-	desplazamiento -= 240;
+	desplazamiento -= 350;
 	for (cuadro in cuadros){
-		cuadros[cuadro].style.transform = `translateY(${desplazamiento}px)`;
+		cuadros[cuadro].style.transform = `translateX(${desplazamiento}px)`;
 	}
 }
 
 /*Adicion de eventos para ejecutar las funciones de arriba respectivamente*/
 
 /*Flechas de la casilla 1*/
-dplzAbajo1.addEventListener("click", ()=>moverAbajo())
+dplzAbajo1.addEventListener("click", ()=>moverDerecha())
 
 /*Flechas de la casilla 2*/
-dplzArriba2.addEventListener("click", ()=>moverArriba());
-dplzAbajo2.addEventListener("click", ()=>moverAbajo());
+dplzArriba2.addEventListener("click", ()=>moverIzquierda());
+dplzAbajo2.addEventListener("click", ()=>moverDerecha());
 
 /*Flechas de la casilla 3*/
-dplzArriba3.addEventListener("click", ()=>moverArriba());
+dplzArriba3.addEventListener("click", ()=>moverIzquierda());
 
 /*De aqui en adelante irá el funcionamiento y expancion de estos cuadros*/
 let infoCuadro1 = document.querySelector(".pagina1__cuadro1--informacion");
@@ -70,14 +77,12 @@ let mostrarInfo = (id)=>{
 		abierto = false;
 		contenedorCuadros.style.height = "240px";
 		cuadros[id].style.height = "240px";
-		setTimeout(()=>{
-			infoCuadros[id].style.display = "none";
-		}, 200)
+		infoCuadros[id].style.display = "none";
 	}
 	else {
 		abierto = true;
-		contenedorCuadros.style.height = "500px";
-		cuadros[id].style.height = "500px";
+		contenedorCuadros.style.height = "450px";
+		cuadros[id].style.height = "450px";
 		infoCuadros[id].style.display = "block";
 	}
 }
@@ -91,3 +96,4 @@ let cerrarTodo = ()=>{
 
 boton1.addEventListener("click", ()=>mostrarInfo(0));
 boton2.addEventListener("click", ()=>mostrarInfo(1));
+boton3.addEventListener("click", ()=>mostrarInfo(2));
